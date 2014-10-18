054b09c065c162f8810a50e8b8c73c3b2be8e7a0
Brian Button <brian.button@centurylinkcloud.com>
2014-10-18 12:34:03 -0500

    Corrected how movies were being output for both methods to provide better experience through web service

--- quote_web_service/routes/movies.rb
+++ quote_web_service/routes/movies.rb
@@ -2,20 +2,22 @@
 require_relative "../models/movie"
 
 get '/api/movies' do
-  movies = Movie.all.to_a
+  movies = Movie.all.to_a.map { |m| "#{m.title}:#{m.quote}" }
 
   format_response(movies, request.accept)
 end
 
 post '/api/movies' do
   body = JSON.parse request.body.read
-  movie = Movie.create(
-    title:    body['title'],
-    quote:    body['quote']
-  )
+  title = body['title']
+  quote = body['quote']
+  puts "body = #{body}"
+  puts "title: #{title}, quote: #{quote}"
+
+  movie = Movie.create title: title, quote: quote
   movie.save
 
   status 201
-  format_response(movie, request.accept)
+  format_response("#{movie.title}:#{movie.quote}", request.accept)
 end
 
